5cb602145221a74d27aea65d1ac1bd18bba46bc3
jackbmontgomery <jackbmontgomery2001@gmail.com>
2025-11-14 09:10:43 +0000

    explicit ignore on ty lsp

--- .config/nvim/lsp/ty.lua
+++ .config/nvim/lsp/ty.lua
@@ -1,7 +1,19 @@
 return {
   cmd = { 'ty', 'server' },
   filetypes = { 'python' },
+  -- these settings are sent to the server by lspconfig
   settings = {
-    ty = { diagnosticMode = 'workspace', disableLanguageServices = true },
+    ty = {
+      diagnosticMode = 'workspace',
+      disableLanguageServices = true,
+    },
   },
+  on_attach = function(client, _)
+    client.server_capabilities.hoverProvider = false
+    client.server_capabilities.definitionProvider = false
+    client.server_capabilities.referencesProvider = false
+    client.server_capabilities.documentSymbolProvider = false
+    client.server_capabilities.codeActionProvider = false
+    client.server_capabilities.completionProvider = nil
+  end,
 }
